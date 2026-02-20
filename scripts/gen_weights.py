a60b8f8f68d550f67e4c376b637501eae8f1f60c
xucaleb6335 <xu.caleb.6335@gmail.com>
2026-02-20 12:41:22 -0800

    test script done

--- scripts/gen_weights.py
+++ scripts/gen_weights.py
@@ -16,17 +16,17 @@ def main():
     #Quantization step:
     #int8_val = Round(Float_Value*127.0 / Max_Abs_value)
 
-    w_max = np.max(weights_float)
-    i_max = np.max(input_float)
+    w_max = np.max(np.abs(weights_float))
+    i_max = np.max(np.abs(input_float))
 
     #cast to int8
     w8 = np.round(weights_float * (127.0/w_max)).astype(np.int8)
-    i8 = np.round(input_float * (127.0/i_max)).asptype(np.int8)
+    i8 = np.round(input_float * (127.0/i_max)).astype(np.int8)
 
     hex_filename = "weights_output.hex"
     with open(hex_filename, "w") as f:
         for val in w8.flatten():
-            f.write(f":{val & 0xFF:02x}\n")
+            f.write(f"{int(val) & 0xFF:02x}\n")
 
 if __name__ == "__main__":
     main()
